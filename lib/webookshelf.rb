0686ebe31ee0eda2fa0e2cb4e95b4e415ff673da
masahino <masahino@gmail.com>
2012-02-17 22:43:59 +0900

    add interface

--- lib/webookshelf.rb
+++ lib/webookshelf.rb
@@ -3,5 +3,15 @@ require 'stack_stock_books.rb'
 require 'mediamarker.rb'
 
 class Webookshelf
+  def initialize(user_id, password)
+  end
+
+  def update(asin, date, state)
+
+  end
+
+  def delete(asin)
+  end
+
   
 end
