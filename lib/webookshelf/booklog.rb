e6f30e7c211b745b463d42ab77d1df23806abffd
masahino <masahino@gmail.com>
2022-06-04 07:52:53 +0900

    change url

--- lib/webookshelf/booklog.rb
+++ lib/webookshelf/booklog.rb
@@ -6,9 +6,9 @@ module Booklog
     require 'mechanize'
     require 'kconv'
 
-    BooklogHomeURI = 'http://booklog.jp'
-    BooklogLoginURI = 'http://booklog.jp/login'
-    BooklogInputURI = 'http://booklog.jp/input'
+    BooklogHomeURI = 'https://booklog.jp'
+    BooklogLoginURI = 'https://booklog.jp/login'
+    BooklogInputURI = 'https://booklog.jp/input'
     def initialize(user_id, password)
       @agent = Mechanize.new
 #      @agent.post_connect_hooks << lambda{|params| params[:response_body] = NKF.nkf('-w8m0', params[:response_body])}
@@ -28,18 +28,17 @@ module Booklog
     # ISBNによる登録
     def input(isbn_list)
       input_page = @agent.get(BooklogInputURI)
-#      input_form = input_page.form_with(:action => BooklogInputURI)
-#      input_form = input_page.form_with(:action => '/input')
+      #      input_form = input_page.form_with(:action => BooklogInputURI)
+      #      input_form = input_page.form_with(:action => '/input')
       input_form = input_page.form_with(:id => 'input_form')
       input_form.isbns = isbn_list.join("\n")
-      input_form.status = "3"
-      input_form.method = "POST"
+      input_form.status = '3'
+      input_form.method = 'POST'
       result_page = input_form.submit
     end
 
     def comment(asin, comment)
-      update_uri = BooklogHomeURI + '/addbook.php?mode=ItemLookup&asin='+asin
-      #p update_uri
+      update_uri = BooklogHomeURI + '/addbook.php?mode=ItemLookup&asin=' + asin
       comment_page = @agent.get(update_uri)
       comment_form = comment_page.form('frm')
       comment_form['comment'] = comment.toeuc
@@ -47,7 +46,6 @@ module Booklog
         puts comment_form['comment']
       end
       result_page = comment_form.submit
-      #puts result_page.body
     end
 
     # 'rank'    "0","1","2","3","4","5"
@@ -55,10 +53,10 @@ module Booklog
     # 'read_at' Date
     # 'description'
     def edit(asin, edit_info)
-      edit_path = '/edit/' + asin
+      edit_path = '/edit/1/' + asin
       edit_uri = BooklogHomeURI + edit_path
       edit_page = @agent.get(edit_uri)
-#      edit_form = edit_page.form(:action => edit_path)
+      #      edit_form = edit_page.form(:action => edit_path)
       edit_form = edit_page.forms[2]
       if edit_info['rank'] 
         edit_form['rank'] = edit_info['rank']
@@ -75,8 +73,8 @@ module Booklog
         edit_form['description'] = edit_info['description']
       end
 
-      edit_form.checkbox_with(:name => "create_on_now").check
-      edit_form['_method'] = "edit"
+      edit_form.checkbox_with(:name => 'create_on_now').check
+      edit_form['_method'] = 'edit'
       edit_form.submit
     end
   end
@@ -107,17 +105,18 @@ if defined?($test) && $test
 
     def test_input
       agent = Booklog::Agent.new(@options['booklog_userid'], @options['booklog_password'])
-#      agent.input(['4480426280'])
-#      agent.input(['4062752638'])
+      #      agent.input(['4480426280'])
+      #      agent.input(['4062752638'])
     end
 
     def test_edit
       agent = Booklog::Agent.new(@options['booklog_userid'], @options['booklog_password'])
       agent.edit('4575513652',
-                 {'rank'=>'4',
-                   'status' => '3', 
-                   'read_at' => Date::parse("2010-08-06"),
-                   'description' => "test"})
-    end          
+                { 'rank' => '4',
+                   'status' => '3',
+                   'read_at' => Date.parse('2010-08-06'),
+                   'description' => 'test' }
+                )
+    end
   end
 end
